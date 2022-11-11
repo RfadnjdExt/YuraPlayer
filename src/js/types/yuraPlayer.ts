fbebdb84a19d6e3267ee47806ea89414a7a55809
XCR90 <yuratimes@gmail.com>
2022-11-11 20:28:01 +0700

    -

--- src/js/types/yuraPlayer.ts
+++
@@ -1,6 +0,0 @@
-type DOMSelector = keyof HTMLElementTagNameMap | 'div#player' | 'ysf-player'
-type Attributes = string | 'class' | 'controls'
-type Class = 'video-stream'
-type AttributesObject = {
-    [key in Attributes]?: Class | ''
-}
--- src/js/yuraPlayer.ts
+++ src/js/yuraPlayer.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
 import dashjs from 'dashjs'
-const $ = (selector: DOMSelector) => document.querySelector(selector)
-const createElement = (tagName: DOMSelector, attributesObject?: AttributesObject) => {
+
+interface Attributes {
+    [key: string]: string
+}
+const $ = (selector: string) => document.querySelector(selector)
+const createElement = (tagName: string, attributesObject?: Attributes) => {
     const el = document.createElement(tagName)
     if(attributesObject) {
         Object.keys(attributesObject).forEach((value) => {
@@ -12,47 +16,14 @@ const createElement = (tagName: DOMSelector, attributesObject?: AttributesObject
 }
 
 const yuraPlayer = async() => {
-
-    const playerContainerOuter = createElement('div')
-    const playerContainerInner = createElement('div')
-    const playerContainer = createElement('div')
-    const ysfPlayer = createElement('ysf-player')
-    const container = createElement('div')
-    const html5VideoPlayer = createElement('div')
-    const html5VideoContainer = createElement('div')
-    const videoStream = createElement('video', { class: 'video-stream', controls: '' })
-
-    videoStream.style.left = '0px'
-    videoStream.style.top = '0px'
-
-    
-    const resize = () => {
-        videoStream.style.width = `${window.innerWidth}px`
-        videoStream.style.height = `${window.innerHeight}px`
-    }
-
-    // const player = dashjs.MediaPlayer().create()
-    // const url = 'https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd'
-    // const startTime = 0
-    // player.initialize(videoStream, url, true, startTime)
-
-    videoStream.setAttribute('src', 'https://jolly-hall-c042.yurasu.workers.dev/?id=1-5UH-KmvkcQ8OcTdCVdtFhaLNdnHHFw7')
-    resize()
-    new ResizeObserver(resize).observe(document.body)
-
-    const yrpGradientBottom = createElement('div')
-    yrpGradientBottom.style.backgroundImage = 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAACSCAYAAACE56BkAAAAAXNSR0IArs4c6QAAAPVJREFUKFNlyOlHGAAcxvHuY93H1n1fW1v3fbej+zAmI5PIRGYiM5JEEkkiiSSRRPoj83nze9Pz4uPrSUh4tURPEpKDFJWKtCBdZSAzeKOykB3kqFzkBfmqAIVBkSrGW7wLSlQpyoJyVYHKoEpVoyaoVXWoDxpUI5qCZtWC98EH1YqPwSfVhvagQ3WiK+hWPegN+lQ/BoJBNYRhjASjagzjwYSaxOfgi/qKb8GUmsZMMKvmMB8sqEUsYRnf8QMr+IlV/MIa1rGB39jEFv7gL7axg3/4j13sYR8HOMQRjnGCU5zhHBe4xBWucYNb3OEeD3jEE55fAOe7I9q0+rDDAAAAAElFTkSuQmCC")'
-
-    html5VideoContainer.appendChild(videoStream)
-    html5VideoContainer.appendChild(yrpGradientBottom)
-    html5VideoPlayer.appendChild(html5VideoContainer)
-    container.appendChild(html5VideoPlayer)
-    ysfPlayer.appendChild(container)
-    playerContainer.appendChild(ysfPlayer)
-    playerContainerInner.appendChild(playerContainer)
-    playerContainerOuter.appendChild(playerContainerInner)
-    $('div#player')?.appendChild(playerContainerOuter)
-    
+    const player = $('#player')!
+    player.setAttribute('class', 'ytd-watch-flexy')
+    const cinematics = createElement('div', { id: 'cinematics', class: 'ytd-watch-flexy'})
+    const playercontainerouter = createElement('div', { id: 'player-container-outer', class: 'ytd-watch-flexy'})
+    const playercontainerinner = createElement('div', { id: 'player-container-inner', class: 'ytd-watch-flexy'})
+    playercontainerouter.appendChild(playercontainerinner)
+    player.appendChild(cinematics)
+    player.appendChild(playercontainerouter)
 }
 
 export default yuraPlayer
