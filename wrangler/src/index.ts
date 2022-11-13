070c6508d79481a5ba4709043aa14d187e8401be
XCR90 <yuratimes@gmail.com>
2022-11-13 18:09:50 +0700

    -

--- wrangler/src/index.ts
+++ wrangler/src/index.ts
@@ -18,15 +18,9 @@ async function updateAccessToken(env: ENV) {
     if(!responseDate) throw Error()
     globalData.expired = new Date(responseDate).getTime() + (jsonData.expires_in * 1000)
     globalData.access_token = `${jsonData.token_type} ${jsonData.access_token}`
-    console.log(globalData)
 }
 
-async function getFile(filePath?: string, fileID?: string) {
-    if(filePath) {
-        filePath.split('/').forEach(async function() {
-            
-        })
-    }
+async function getFile(fileID: string) {
     const response = await fetch(`https://www.googleapis.com/drive/v3/files/${fileID}?` + new URLSearchParams({
         alt: 'media',
         supportsAllDrives: 'true'
@@ -35,32 +29,46 @@ async function getFile(filePath?: string, fileID?: string) {
             'Authorization': globalData.access_token
         }
     })
-    return new Response(response.body, {
-        headers: response.headers
-    })
+    return new Response(response.body)
 }
 
 export default {
     async fetch(request: Request, env: ENV) {
         const url = new URL(request.url)
-        if(url.pathname === '/') return new Response('<script src="/script.js">')
-        if(url.pathname === '/script.js') return await getFile
-        if(url.pathname === '/favicon.ico') return new Response('', { status: 404 })
         if(!globalData.expired || globalData.expired < new Date().getTime()) await updateAccessToken(env)
+        if(url.pathname === '/') return new Response(`<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <script src="script.js"></script>
+        </body>
+        </html>`, {
+            headers: {
+                'Content-Type': 'text/html'
+            }
+        })
+        if(url.pathname === '/script.js') return await getFile('1Aa_poscRcW2KQFH_OUXbUpG-Cg7knDE7')
+        if(url.pathname === '/favicon.ico') return new Response((await fetch('https://www.yurasu.xyz/favicon.ico')).body)
+        return await getFile(url.pathname.slice(1))
         // const response = await fetch('https://www.googleapis.com/drive/v3/files?' + new URLSearchParams({
         //     corpora: 'drive',
         //     driveId: '0AKOrYgWhtV5YUk9PVA',
         //     includeItemsFromAllDrives: 'true',
         //     orderBy: 'name',
-        //     pageSize: '46',
-        //     q: '"0AKOrYgWhtV5YUk9PVA" in parents and trashed = false',
+        //     q: '"0AKOrYgWhtV5YUk9PVA" in parents and trashed = false and name = "Kusonime - Download Anime Batch Subtitle Indonesia Komplit (Ceritanya).url"',
         //     supportsAllDrives: 'true'
         // }), {
         //     headers: {
         //         Authorization: globalData.access_token
         //     }
         // })
-        // const responseData = await response.text()
-        // return new Response(responseData, { headers: { 'content-type': response.headers.get('content-type')! } })
+        // console.log((await response.text()))
+        // return response
+        // return await getFile(url.pathname.slice(1))
     }
 }
