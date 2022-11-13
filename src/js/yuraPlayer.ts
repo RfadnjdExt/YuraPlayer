871fb72c1fe6acbc206e7540d34259019e773d3e
XCR90 <yuratimes@gmail.com>
2022-11-13 08:44:52 +0700

    -

--- src/js/yuraPlayer.ts
+++ src/js/yuraPlayer.ts
@@ -1,10 +1,11 @@
 import axios from 'axios'
 import dashjs from 'dashjs'
+import SubtitleOctopus from './subtitles-octopus'
 
 interface Attributes {
     [key: string]: string
 }
-const $ = (selector: string) => document.querySelector(selector)
+const $ = (selector: string): HTMLElement | null => document.querySelector(selector)
 const createElement = (tagName: string, attributesObject?: Attributes) => {
     const el = document.createElement(tagName)
     if(attributesObject) {
@@ -16,7 +17,18 @@ const createElement = (tagName: string, attributesObject?: Attributes) => {
 }
 
 const yuraPlayer = async() => {
-    dashjs.MediaPlayer().create().initialize($('video')!)
+    const video = $('video')!
+    dashjs.MediaPlayer().create().initialize(video, './streams/manifest.mpd', false)
+    new SubtitleOctopus({
+        video,
+        subUrl: './subtitle.br',
+        workerUrl: 'subtitles-octopus-worker.js',
+        fonts: (await axios.post('./fonts', {"q":"","password":null,"page_token":null,"page_index":0})).data.data.files.map((font: {name: string}) => `./fonts/${font.name}`),
+        lazyFileLoading: true,
+        debug: true,
+        renderMode: 'lossy'
+
+    })
 }
 
 export default yuraPlayer
--- wrangler/src/index.ts
+++ wrangler/src/index.ts
@@ -1,56 +1,6 @@
-interface ExecutionContext {}
-
-interface AuthConfig {
-    accessToken: string,
-    enable_cors_file_down: true,
-    expires: number
-}
-
-interface Env {
-    CLIENT_ID: string
-    CLIENT_SECRET: string
-    REFRESH_TOKEN: string
-}
-
-const authConfig = {} as AuthConfig
-
 export default {
-    async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-        if(authConfig.expires == undefined || authConfig.expires < Date.now()) {
-            const post_data = {
-                client_id: env.CLIENT_ID,
-                client_secret: env.CLIENT_SECRET,
-                refresh_token: env.REFRESH_TOKEN,
-                grant_type: "refresh_token"
-            }
-            const ret = [] as string[]
-            for(let d in post_data) {
-                ret.push(`${encodeURIComponent(d)}=${encodeURIComponent(post_data[d])}`)
-                
-            }
-            const response = await fetch('https://www.googleapis.com/oauth2/v4/token', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: ret.join('&')
-            })
-            const obj = await response.json()
-
-            if(obj.access_token != undefined) {
-                authConfig.accessToken = obj.access_token
-                authConfig.expires = Date.now() + 3500 * 1000
-            }
-        }
-        let res = await fetch(`https://www.googleapis.com/drive/v3/files/${new URL(request.url).searchParams.get('id')}?alt=media`, {
-            headers: {
-                'Authorization': `Bearer ${authConfig.accessToken}`
-            },
-            method: 'GET'
-        })
-        const { headers } = (res = new Response(res.body, res))
-        authConfig.enable_cors_file_down && headers.append('Access-Control-Allow-Origin', '*')
-        headers.set('Content-Disposition', 'inline')
-        return res
+    async fetch(request: Request) {
+        console.log(request.constructor.name)
+        return new Response('Hello World!')
     }
 }
